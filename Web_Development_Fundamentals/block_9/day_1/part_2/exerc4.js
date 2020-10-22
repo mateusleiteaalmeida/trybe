5038987fcdcaa8ba5aec7ae4ecaf7de84bfd52a9
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-10-22 18:16:05 -0300

    Exercicio 5 - Parte 2 - Dia 1 - Bloco 9

--- Web_Development_Fundamentals/block_9/day_1/part_2/exerc4.js
+++ Web_Development_Fundamentals/block_9/day_1/part_2/exerc4.js
@@ -8,7 +8,7 @@ const getMarsTemperature = () => {
 // crie a função sendMarsTemperature abaixo
 const sendMarsTemperature = () => {
     const currentTemperature  = getMarsTemperature();
-    setTimeout(() => console.log(`Mars temperature is: ${currentTemperature} degree Celsius`), 5000);
+    setTimeout(() => console.log(`Mars temperature is: ${currentTemperature} degree Celsius`), messageDelay());
 }
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
