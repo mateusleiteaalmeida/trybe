5a3da77f28cff43f1eb16bd7b281a8f656f6ea6c
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-04 23:02:45 -0300

    Exercicio 2 - Dia 2 - Bloco 10 - p.1

---
+++ Web_Development_Fundamentals/block_10/day_2/exerc2.test.js
@@ -0,0 +1,25 @@
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+      
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      }
+      
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+}
+      
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+}
+  
+describe('getUserName', () => {
+    test('if the user is found', () => {
+      expect.assertions(1);
+      return getUserName(4).then(res => expect(res).toEqual('Mark'));
+  });
+});
