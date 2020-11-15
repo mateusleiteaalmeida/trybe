bf03060a40c1b4d371d30ab1857e402a7a13bd9f
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-14 23:05:20 -0300

    Exercicio 1 - Dia 2 - Bloco 12 - p.2

--- Front-end/block_12/day_2/my-form-2.0/src/App.js
+++ Front-end/block_12/day_2/my-form-2.0/src/App.js
@@ -1,5 +1,4 @@
-import { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
@@ -7,53 +6,46 @@ class App extends Component {
       <div>
         <h1>My Form</h1>
         <form>
+          <fieldset>
+            <label>
+              Nome
+              <input name="name" maxLength="40" required />
+            </label>
 
-          <label>
-            Nome
-            <input name="name" maxLength="40" required>
-            </input>
-          </label>
+            <label>
+              Email
+              <input name="email" maxLength="50" required />
+            </label>
 
-          <label>
-            Email
-            <input name="email" maxLength="50" required>
-            </input>
-          </label>
+            <label>
+              CPF
+              <input name="cpf" maxLength="11" required />
+            </label>
 
-          <label>
-            CPF
-            <input name="cpf" maxLength="11" required>
-            </input>
-          </label>
+            <label>
+              Endereço
+              <input name="adress" maxLength="200" required />
+            </label>
 
-          <label>
-            Endereço
-            <input name="adress" maxLength="200" required>
-            </input>
-          </label>
+            <label>
+              Cidade
+              <input name="city" maxLength="28" required />
+            </label>
 
-          <label>
-            Cidade
-            <input name="city" maxLength="28" required>
-            </input>
-          </label>
+            <label>
+              Estado
+              <input name="state" required />
+            </label>
 
-          <label>
-            Estado
-            <input name="state" required>
-            </input>
-          </label>
+            <label>
+              Tipo da residência
+              <input type="radio" />
+                <label>Casa</label>
+              <input type="radio" />
+                <label>Apartamento</label>
+            </label>
 
-          <label>
-            Tipo da residência
-            <input type="radio">
-              <label>Casa</label>
-            </input>
-            <input type="radio">
-              <label>Apartamento</label>
-            </input>
-          </label>
-
+          </fieldset>
         </form>
       </div>
     )
