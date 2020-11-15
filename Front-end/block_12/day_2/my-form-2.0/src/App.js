96217cedc6623116077f86b4905a465af7d9f2ab
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-14 22:50:25 -0300

    Exercicio 1 - Dia 2 - Bloco 12

--- Front-end/block_12/day_2/my-form-2.0/src/App.js
+++ Front-end/block_12/day_2/my-form-2.0/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Form</h1>
+        <form>
+
+          <label>
+            Nome
+            <input name="name" maxLength="40" required>
+            </input>
+          </label>
+
+          <label>
+            Email
+            <input name="email" maxLength="50" required>
+            </input>
+          </label>
+
+          <label>
+            CPF
+            <input name="cpf" maxLength="11" required>
+            </input>
+          </label>
+
+          <label>
+            Endereço
+            <input name="adress" maxLength="200" required>
+            </input>
+          </label>
+
+          <label>
+            Cidade
+            <input name="city" maxLength="28" required>
+            </input>
+          </label>
+
+          <label>
+            Estado
+            <input name="state" required>
+            </input>
+          </label>
+
+          <label>
+            Tipo da residência
+            <input type="radio">
+              <label>Casa</label>
+            </input>
+            <input type="radio">
+              <label>Apartamento</label>
+            </input>
+          </label>
+
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
