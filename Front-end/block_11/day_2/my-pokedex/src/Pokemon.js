587b52caad30010b21c94e99800b1c55a8cbf5b8
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-20 22:37:03 -0300

    Exercicio - Dia 2 - Bloco 11 - conclusao dos requisitos para renderizacao

--- Front-end/block_11/day_2/my-pokedex/src/Pokemon.js
+++ Front-end/block_11/day_2/my-pokedex/src/Pokemon.js
@@ -6,7 +6,7 @@ class Pokemon extends React.Component {
     const { name, type, averageWeight, image } = properties.pokemons;
     return (
         <div>
-            <img src={image} alt="" />
+            <img src={image} alt={`${name}`} />
             <p>{name}</p>
             <p>{type}</p>
             <p>Average weight: {averageWeight.value}{averageWeight.measurementUnit}</p>
