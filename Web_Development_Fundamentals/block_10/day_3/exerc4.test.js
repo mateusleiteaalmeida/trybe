096b09b20d3a5d9dfbd08c176ef12efea8d85b2c
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-23 22:25:31 -0300

    Exercicio 4 - Dia 3 - Bloco 10

---
+++ Web_Development_Fundamentals/block_10/day_3/exerc4.test.js
@@ -0,0 +1,40 @@
+function upperCase(string) {
+    string.toUpperCase();
+};
+
+function firstLetter(string) {
+    string.charAt(0)
+};
+
+function concatenate(string1, string2) {
+    string1.concat(string2)
+};
+
+describe("testing functions with strings", () => {
+  test("mocking function to receive a string and return it in lower case", () => {
+    upperCase = jest.fn().mockImplementation(string => string.toLowerCase());
+
+    expect(upperCase("STRING")).toBe("string");
+    expect(upperCase).toHaveBeenCalled();
+    expect(upperCase).toHaveBeenCalledTimes(1);
+    expect(upperCase).toHaveBeenCalledWith("STRING");
+  });
+  
+  test("mocking function to receive a string and return it's first letter", () => {
+    firstLetter = jest.fn().mockImplementation(string => string.charAt(string.length - 1));
+
+    expect(firstLetter("string")).toBe("g");
+    expect(firstLetter).toHaveBeenCalled();
+    expect(firstLetter).toHaveBeenCalledTimes(1);
+    expect(firstLetter).toHaveBeenCalledWith("string");
+  });
+ 
+  test("mocking function to receive three strings and concatanate them", () => {
+    concatenate = jest.fn().mockImplementation((string1, string2, string3) => string1.concat(string2, string3));
+
+    expect(concatenate("a", "b", "c")).toBe("abc");
+    expect(concatenate).toHaveBeenCalled();
+    expect(concatenate).toHaveBeenCalledTimes(1);
+    expect(concatenate).toHaveBeenCalledWith("a", "b", "c");
+  });
+});
