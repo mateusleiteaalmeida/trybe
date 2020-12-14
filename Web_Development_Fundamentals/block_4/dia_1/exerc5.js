475df6b45bf8a9241b17f284328917fc9679c828
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-12-14 20:26:18 -0300

    Organizaçao das pastas e arquivos

--- Web_Development_Fundamentals/block_4/dia_1/exerc5.js
+++
@@ -1,9 +0,0 @@
-let num1 = 75;
-let num2 = 45;
-let num3 = 60;
-
-if (num1 + num2 + num3 === 180){
-    console.log("true")
-} else {
-    console.log("false")
-};
--- Web_Development_Fundamentals/block_4/dia_2/exerc9.js
+++
@@ -1,9 +0,0 @@
-let count = [];
-
-for (let i = 0; i <= 25; i++){
-    count.push(i);
-};
-
-for (let i = 0; i < count.length; i++){
-    console.log(count[i] / 2)
-}
