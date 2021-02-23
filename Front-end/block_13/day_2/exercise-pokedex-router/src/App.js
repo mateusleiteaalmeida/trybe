4fa89ba51164745cf110ba92d898d1b6f4178a2a
mateus <mateuslaalmeida@gmail.com>
2021-02-23 00:08:17 -0300

    Exercicio 7 - Dia 2 - Bloco 13

--- Front-end/block_13/day_2/exercise-pokedex-router/src/App.js
+++ Front-end/block_13/day_2/exercise-pokedex-router/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 import PokemonDetails from './PokemonDetails';
 import About from './About';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-			<Route exact path="/" component={() => <Pokedex pokemons={ pokemons }/>} />
-			<Route path="/pokemons/:id" component={ PokemonDetails } />
-			<Route path="/about" component={ About } />
+			<Switch>
+				<Route exact path="/" component={() => <Pokedex pokemons={ pokemons }/>} />
+				<Route path="/pokemons/:id" component={ PokemonDetails } />
+				<Route path="/about" component={ About } />
+				<Route component={ NotFound } />
+			</Switch>
 		</BrowserRouter>
   );
 }
